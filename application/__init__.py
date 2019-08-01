e1198a6b1c138376320840be44c97e639132e6ef
Otto Ahoniemi <43019238+otahontas@users.noreply.github.com>
2019-08-01 19:13:36 +0300

    lisätty notes-model ja notes-taulun automaattinen alustus, mikäli ko. taulu on tyhjä

--- application/__init__.py
+++ application/__init__.py
@@ -13,4 +13,23 @@ from application import views
 from application.chords import models
 from application.chords import views
 
+from application.notes import models
+from application.notes.models import Note
+
 db.create_all()
+
+#n = Note("C")
+#db.session().add(n)
+#db.session().commit()
+
+# If there's no items in notes database, initialize database
+if not Note.query.all():
+    with open("application/notes/notesfordatabase.txt") as f:
+        for line in f:
+            n = Note(line)
+            db.session().add(n)
+    db.session().commit()
+    if Note.query.count() == 21:
+        print("SUCCESS: Notes-table successfully initialized")
+    else:
+        print("ERROR: There was an error when initializing notes-table")
